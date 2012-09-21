4a01b1791ef4fb3dc5b84bbadbc1ab95b4740e45
Ryan Johnson <ryan.txanson@gmail.com>
2012-09-21 16:48:45 -0500

    Fetch the car, Richard...

---
+++ public/javascripts/app.js
@@ -0,0 +1,259 @@
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    definition(module.exports, localRequire(name), module);
+    var exports = cache[name] = module.exports;
+    return exports;
+  };
+
+  var require = function(name) {
+    var path = expand(name, '.');
+
+    if (has(cache, path)) return cache[path];
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex];
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '"');
+  };
+
+  var define = function(bundle) {
+    for (var key in bundle) {
+      if (has(bundle, key)) {
+        modules[key] = bundle[key];
+      }
+    }
+  }
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.brunch = true;
+})();
+
+window.require.define({"application": function(exports, require, module) {
+  var Application, HelloView, Router;
+
+  Router = require('routers/router');
+
+  HelloView = require('views/hello_view');
+
+  module.exports = Application = (function() {
+
+    function Application() {
+      var _this = this;
+      $(function() {
+        _this.initialize();
+        return Backbone.history.start({
+          pushState: true
+        });
+      });
+    }
+
+    Application.prototype.initialize = function() {
+      this.router = new Router;
+      return this.helloView = new HelloView;
+    };
+
+    return Application;
+
+  })();
+
+  window.app = new Application;
+  
+}});
+
+window.require.define({"models/collection": function(exports, require, module) {
+  var Collection,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  module.exports = Collection = (function(_super) {
+
+    __extends(Collection, _super);
+
+    function Collection() {
+      return Collection.__super__.constructor.apply(this, arguments);
+    }
+
+    Collection.prototype.model = require('./model');
+
+    return Collection;
+
+  })(Backbone.Collection);
+  
+}});
+
+window.require.define({"models/model": function(exports, require, module) {
+  var Model,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  module.exports = Model = (function(_super) {
+
+    __extends(Model, _super);
+
+    function Model() {
+      return Model.__super__.constructor.apply(this, arguments);
+    }
+
+    return Model;
+
+  })(Backbone.Model);
+  
+}});
+
+window.require.define({"routers/router": function(exports, require, module) {
+  var Router,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  module.exports = Router = (function(_super) {
+
+    __extends(Router, _super);
+
+    function Router() {
+      return Router.__super__.constructor.apply(this, arguments);
+    }
+
+    Router.prototype.routes = {
+      '': 'index',
+      '*all': 'redirect'
+    };
+
+    Router.prototype.index = function() {
+      return $('#content').append(app.helloView.render().el);
+    };
+
+    Router.prototype.redirect = function() {
+      return this.navigate('');
+    };
+
+    return Router;
+
+  })(Backbone.Router);
+  
+}});
+
+window.require.define({"views/hello_view": function(exports, require, module) {
+  var HelloView,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  module.exports = HelloView = (function(_super) {
+
+    __extends(HelloView, _super);
+
+    function HelloView() {
+      return HelloView.__super__.constructor.apply(this, arguments);
+    }
+
+    HelloView.prototype.className = 'hello';
+
+    HelloView.prototype.template = require('./templates/hello');
+
+    HelloView.prototype.render = function() {
+      this.$el.html(this.template);
+      return this;
+    };
+
+    return HelloView;
+
+  })(Backbone.View);
+  
+}});
+
+window.require.define({"views/templates/hello": function(exports, require, module) {
+  module.exports = function (__obj) {
+    if (!__obj) __obj = {};
+    var __out = [], __capture = function(callback) {
+      var out = __out, result;
+      __out = [];
+      callback.call(this);
+      result = __out.join('');
+      __out = out;
+      return __safe(result);
+    }, __sanitize = function(value) {
+      if (value && value.ecoSafe) {
+        return value;
+      } else if (typeof value !== 'undefined' && value != null) {
+        return __escape(value);
+      } else {
+        return '';
+      }
+    }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+    __safe = __obj.safe = function(value) {
+      if (value && value.ecoSafe) {
+        return value;
+      } else {
+        if (!(typeof value !== 'undefined' && value != null)) value = '';
+        var result = new String(value);
+        result.ecoSafe = true;
+        return result;
+      }
+    };
+    if (!__escape) {
+      __escape = __obj.escape = function(value) {
+        return ('' + value)
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;');
+      };
+    }
+    (function() {
+      (function() {
+      
+        __out.push('<a href="http://brunch.io">\n  <img src="images/brunch.png">\n</a>\n');
+      
+      }).call(this);
+      
+    }).call(__obj);
+    __obj.safe = __objSafe, __obj.escape = __escape;
+    return __out.join('');
+  }
+}});
+
